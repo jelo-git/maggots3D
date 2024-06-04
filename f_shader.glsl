52ae6bb18143e0ce44798e58379db87923d9d6a4
jelo-git <batogfilip@gmail.com>
2024-06-04 16:40:44 +0200

    direct light and point light

--- f_shader.glsl
+++ f_shader.glsl
@@ -4,25 +4,57 @@ uniform vec4 lightColor;
 uniform vec3 lightPos;
 uniform vec3 viewPos;
 
-in vec3 fragNormal;
-in vec3 fragPos;
+uniform sampler2D tex0;
+uniform sampler2D tex1;
+
+in vec3 texCoord;
+in vec3 normal;
+in vec3 position;
 
 out vec4 color;
+
+vec4 pointLight(){
+	//ambient
+	float ambient = 0.2;
+
+	//light vec
+	vec3 lightvec = lightPos - position;
+
+	//diffuse
+	vec3 n = normalize(normal);
+	vec3 lightDir = normalize(lightvec);
+	float diff = max(dot(n, lightDir), 0.0);
+
+	//specular
+	float specularStrength = 0.5;
+	vec3 viewDir = normalize(viewPos - position);
+	vec3 reflectDir = reflect(-lightDir, n);
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 4) * specularStrength;
+
+	//intensity
+	float distance = length(lightvec);
+	float a = 2.5;
+	float b = 0.6;
+	float intensity = 1.0 / (length(lightvec) * length(lightvec));
+
+	//combine results
+	return (texture(tex0, texCoord) * (diff*intensity+ambient) + texture(tex1,texCoord).r * spec*intensity) * lightColor;
+}
 
 vec4 directLight(){
 	//ambient
 	float ambient = 0.2;
 
 	//diffuse
-	vec3 normal = normalize(fragNormal);
+	vec3 n = normalize(normal);
 	vec3 posOfLight = vec3(9999.0f,9999.0f,0.0f);
 	vec3 lightDir = normalize(vec3(posOfLight));
-float diff = max(dot(normal, lightDir), 0.0);
+	float diff = max(dot(n, lightDir), 0.0);
 
 	//specular
 	float specularStrength = 0.5;
-	vec3 viewDir = normalize(viewPos - fragPos);
-	vec3 reflectDir = reflect(-lightDir, normal);
+	vec3 viewDir = normalize(viewPos - position);
+	vec3 reflectDir = reflect(-lightDir, n);
 	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 2) * specularStrength;
 
 	//combine results
@@ -30,18 +62,5 @@ float diff = max(dot(normal, lightDir), 0.0);
 }
 
 void main() {
-	// diffuse
-	vec3 normal = normalize(fragNormal);
-	vec3 lightDir = normalize(lightPos - fragPos);
-
-	float diff = max(dot(normal, lightDir), 0.0);
-
-	// specular
-	float specularStrength = 0.5;
-	vec3 viewDir = normalize(viewPos - fragPos);
-	vec3 reflectDir = reflect(-lightDir, normal);
-	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 4) * specularStrength;
-
-	// combine results
-	color = vec4(1.0, 1.0, 1.0, 1.0) * lightColor * (diff + spec);
+	color = directLight();
 }
--- v_shader.glsl
+++ v_shader.glsl
@@ -1,21 +1,24 @@
 #version 330
 
-layout(location = 0) in vec3 pos;
-layout(location = 1) in vec3 normal;
+layout(location = 0) in vec3 iPosition;
+layout(location = 1) in vec3 iNormal;
+layout(location = 2) in vec2 iTexCoord;
 
 uniform mat4 M;
 uniform mat4 V;
 uniform mat4 P;
 
-out vec3 fragNormal;
-out vec3 fragPos;
+out vec3 normal;
+out vec3 position;
+out vec2 texCoord;
 
 
 void main(){
-    vec4 vertex = vec4(pos, 1.0);
+    vec4 vertex = vec4(iPosition, 1.0);
 
-    fragNormal = normal;
-    fragPos = vec3(M * vertex);
+    normal = iNormal;
+    position = vec3(M * vertex);
+    texCoord = iTexCoord;
 
     gl_Position = P * V * M * vertex;
 }
